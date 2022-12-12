f02ed330573656fca274878acb987117931cc162
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-12-12 11:02:54 -0800

    Update Configs.php

--- Configs.php
+++ Configs.php
@@ -33,6 +33,7 @@
             <div class="portfolio-info">
               <h3>Project information</h3>
               <ul>
+                <li><strong>Category</strong>: Web App</li>
               
             </div>
             <div class="portfolio-description">
