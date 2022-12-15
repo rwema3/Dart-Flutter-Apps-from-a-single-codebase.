2e25f12d712dcfdccbe7290803a8c2656f5bb69f
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-12-15 20:29:09 +0200

    ER

--- Configs.php
+++ Configs.php
@@ -4,9 +4,6 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
 
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 
 </head>
 
@@ -60,7 +57,7 @@
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
   <script src="assets/vendor/purecounter/purecounter.js"></script>
- 
+
 </body>
 
 </html>
