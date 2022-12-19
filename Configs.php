d0d63a72a241ecb804c3be4d0e786856ef372978
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-12-19 15:24:21 +0200

    BN

--- Configs.php
+++ Configs.php
@@ -12,7 +12,8 @@
 
           <div class="swiper-slide">
                   <img src="assets/img/portfolio/portfolio-details-1.png" alt="">
-                
+                </div>
+
               </div>
               <div class="swiper-pagination"></div>
             </div>
