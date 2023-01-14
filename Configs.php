a8de92ad4d130be54fa52569555ef7f024ad826d
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-01-14 19:03:34 +0200

    Update Configs.php

--- Configs.php
+++ Configs.php
@@ -69,6 +69,3 @@
                    function __construct(argument)
                    {
                      // code...
-                   }
-                 }
-                 '] = something;'; = ['' => , ];] as $key => $value): ?>
