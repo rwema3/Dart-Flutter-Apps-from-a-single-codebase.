1044828dcf173abef59d1826e0d33d4162b9c344
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-01-14 19:04:16 +0200

    Update Configs.php

--- Configs.php
+++ Configs.php
@@ -76,3 +76,29 @@
              <?php endforeach; ?></var>
 
             <div class="portfolio-description">
+
+              <strong>Description</strong>
+              <p>NowPay is a financial-wellness platform for employees in emerging markets. NowPay enables the workforce to better manage their budgets, overcome cash flow problems and avoid unexpected events by offering instant cash advances backed by the employee’s monthly salary.
+              </p                                >
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+  </main>
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+
+</body>
+
+</html>
