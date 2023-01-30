f70f7a28e039a7376ce2c821ec52b551eccb612b
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-01-30 22:01:14 +0200

    Update Configs.php

--- Configs.php
+++ Configs.php
@@ -1,6 +1,9 @@
 <html lang="en">
 
-              
+              ="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+
 </body>
 
 </html>
