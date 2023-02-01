eb9763dedc09c9038c54fd80190d554ac8cb03dd
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-02-01 14:16:22 +0200

    Update Configs.php

--- Configs.php
+++ Configs.php
@@ -36,9 +36,6 @@
 
         </div>
 
-      </div>
-    </section>
-
  
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
