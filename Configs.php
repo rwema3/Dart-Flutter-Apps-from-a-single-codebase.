93d5feddac8757cb8ebdb3e6abb956a73827403d
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-02-03 13:45:51 +0200

    Update Configs.php

--- Configs.php
+++ Configs.php
@@ -34,3 +34,5 @@
   <a href="#" class="bac-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
   <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
