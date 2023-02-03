5ede708fb72cecfa14533e1b12531168a8d96744
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-02-03 13:45:25 +0200

    Update Configs.php

--- Configs.php
+++ Configs.php
@@ -29,18 +29,7 @@
             <div class="portfolio-description">
 
               <strong>Description</strong>
-              <p>NowPay is a financial-wellness platform for employees in emerging markets. NowPay enables the workforce to better manage their budgets, overcome cash flow problems and avoid unexpected events by offering instant cash advances backed by the employee’s monthly salary.
-              </p                                >
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section>
-
-  </main>
-
+        
   <div id="preloader"></div>
   <a href="#" class="bac-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
   <script src="assets/vendor/aos/aos.js"></script>
